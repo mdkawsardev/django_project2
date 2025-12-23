d66170f8f965608b13a6631a1d1b6bcb38ec25bd
mdkawsar <info.mdkawsardev@gmail.com>
2025-12-23 19:43:22 +0600

    django is running

---
+++ website/pull/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('project1/', views.project1, name='project1'),
+]
---
+++ website/pull/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def project1(request):
+    return HttpResponse("Hello! This is my first app")
+
+# Create your views here.
