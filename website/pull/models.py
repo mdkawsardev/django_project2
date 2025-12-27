40d94cdb82a9bcc98d1964a8b89f4721d4a64ec3
mdkawsar <info.mdkawsardev@gmail.com>
2025-12-27 18:21:50 +0600

    django is running

--- website/pull/models.py
+++ website/pull/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class questions(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('Date published')
+
+class choice(models.Model):
+    question = models.ForeignKey(questions, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
