41c3a1b944a6c733dcbc2448cd00ea6ced77bdc3
mdkawsar <info.mdkawsardev@gmail.com>
2025-12-28 21:04:49 +0600

    django is running

--- website/pull/views.py
+++ website/pull/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 def project1(request):
     return HttpResponse("Hello! This is my first app")
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+
 # Create your views here.
--- website/website/settings.py
+++ website/website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pull.apps.PullConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
