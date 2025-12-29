d0a8ae2c2afad4bcd41dff50ebc4cd7cff30f04c
mdkawsar <info.mdkawsardev@gmail.com>
2025-12-29 13:55:56 +0600

    django is running

--- website/pull/views.py
+++ website/pull/views.py
@@ -13,6 +13,5 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+
 # Create your views here.
