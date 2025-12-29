c5237f6299d898ad88542ae0bae0f3e8836b925e
mdkawsar <info.mdkawsardev@gmail.com>
2025-12-29 16:35:24 +0600

    django is running

--- website/pull/views.py
+++ website/pull/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("This is home page. I'm here")
+    return render(request, 'index.html')
+    # return HttpResponse("This is home page. I'm here")
 def about(request):
     return HttpResponse("This is about page")
 def contact(request):
