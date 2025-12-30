2c22a3125f582378dda7cb9d15b16ff3b8c46c0d
mdkawsar <info.mdkawsardev@gmail.com>
2025-12-30 14:27:38 +0600

    django is running

--- website/website/urls.py
+++ website/website/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 urlpatterns = [
-    # path('pull/', include('pull.urls')),
     path('', include('pull.urls')),
     path("admin/", admin.site.urls),
 ]
