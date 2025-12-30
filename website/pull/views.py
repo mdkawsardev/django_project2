410386e70da74bc9479bfafc0ab88abf83aa9a8f
mdkawsar <info.mdkawsardev@gmail.com>
2025-12-30 11:56:58 +0600

    django is running

--- website/pull/views.py
+++ website/pull/views.py
@@ -8,3 +8,5 @@ def about(request):
     return render(request, 'about.html')
 def contact(request):
     return render(request, 'contact.html')
+def blog(request):
+    return render(request, 'blog.html')
