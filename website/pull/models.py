a32c318c997fc5f175bc9b2156bed4d9c831c2c3
mdkawsar <info.mdkawsardev@gmail.com>
2025-12-30 21:50:06 +0600

    django is running

--- website/pull/models.py
+++ website/pull/models.py
@@ -7,3 +7,5 @@ class Contact(models.Model): #? I created a model named Contact
     phone = models.CharField(max_length=11)
     desc = models.TextField()
     date = models.DateField()
+    def __str__(self): #? To show name in admin
+        return self.name
--- website/pull/views.py
+++ website/pull/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from pull.models import Contact
 from datetime import datetime
-
+from django.contrib import messages
+context = {
+    'records': Contact.objects.all()
+}
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
     # return HttpResponse("This is home page. I'm here")
 def about(request):
     return render(request, 'about.html')
@@ -16,6 +19,8 @@ def contact(request):
         desc = request.POST.get('desc')
         contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
         contact.save()
+        messages.success(request, "Your form has been sent!")
     return render(request, 'contact.html')
 def blog(request):
     return render(request, 'blog.html')
+
